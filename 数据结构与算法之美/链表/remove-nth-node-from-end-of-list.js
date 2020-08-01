36331ee379f91568d9d3e543099728494220cf9f
juss <juss@gitblit.geo-compass.com>
2020-08-01 20:47:56 +0800

    修改:  优化代码, delNode 直接delete 不需要再遍历

--- 数据结构与算法之美/链表/remove-nth-node-from-end-of-list.js
+++ 数据结构与算法之美/链表/remove-nth-node-from-end-of-list.js
@@ -4,39 +4,33 @@ function ListNode(val, next) {
 }
 
 
-// 找到要删除节点的前一个节点
 function findNthBeforeNodeFromEnd(head, n) {
   let cur = 1
   let nthNode = head
-  while (head != null) {
-    if (cur > n) {
-      nthNode = nthNode.next
-    }
-    head = head.next
+  let fast = head
+  while (cur <= n) {
     cur++
+    fast = fast.next
+  }
+
+  while (fast != null) {
+    fast = fast.next
+    nthNode = nthNode.next
   }
   return nthNode
 }
 
-function deleteNode(head, delNode) {
-  let result = head
-  while (head != null) {
-    if (head.next == delNode) {
-      head.next = delNode.next
-      return result
-    }
-
-    head = head.next
-  }
+function deleteNode(delNode) {
+  delNode.next = delNode.next.next
 }
 
 var removeNthFromEnd = function (head, n) {
   // 给head 加一个pre , 为了方便删除
   let pre = new ListNode(-1, head)
   // 先找到倒数第n个的前一个节点
-  let delNode = findNthBeforeNodeFromEnd(head, n)
+  let delNode = findNthBeforeNodeFromEnd(pre, n + 1)
   // 删除这个
-  deleteNode(pre, delNode)
+  deleteNode(delNode)
   return pre.next
 };
 
