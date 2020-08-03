c746c35e70276802e88568e0183b2bf81703fbec
juss <juss@gitblit.geo-compass.com>
2020-08-03 22:14:31 +0800

    新增: 求最小元素 新增: 基础计算表达式

--- 数据结构与算法之美/栈/basic-calculator.js
+++ 数据结构与算法之美/栈/basic-calculator.js
@@ -1,21 +1,81 @@
+// https://leetcode-cn.com/problems/basic-calculator/
+// 224. 基本计算器
+
+function isNumber(chr) {
+  let n = Number(chr)
+  if (isNaN(n)) {
+    return false
+  }
+  return true
+}
+
+
+function handleAddOrSub(num, opt, chr) {
+  let isPush = opt[opt.length - 1] == '('
+  if (isPush) {
+    opt.push(chr)
+    return
+  }
+
+  handleCal(num, chr)
+}
+
+function handleCal(num, chr) {
+  let n2 = num.pop()
+  let n1 = num.pop()
+  if (chr == '-') {
+    num.push(n1 - n2)
+  }
+  if (chr == '+') {
+    num.push(n1 + n2)
+  }
+}
+
+function handle(num, opt, chr) {
+  if (chr == '(') {
+    opt.push(chr)
+    return
+  }
+  if (chr == ')') {
+    while (opt.length > 0) {
+      let popChr = opt.pop()
+      if (popChr == '(') {
+        return
+      }
+      handleCal(num, popChr)
+    }
+  }
+  if (opt[opt.length - 1] == '(') {
+    opt.push(chr)
+    return
+  }
+  handleCal(num, opt.pop())
+  opt.push(chr)
+}
+
 /**
  * @param {string} s
  * @return {number}
  */
 var calculate = function (s) {
   let expr = s.replace(' ', '')
-  
-
   let opt = []
   let num = []
 
   for (let i = 0; i < expr.length; i++) {
-    if (isNumber()) {
-
-    } else {
-
+    let chr = expr[i]
+    if (isNumber(chr)) {
+      num.push(Number(chr))
+      continue
     }
-
+    handle(num, opt, chr)
   }
 
+  while (opt.length != 0) {
+    handleCal(num, opt.pop())
+  }
+  return num[0]
 };
+
+
+calculate("(1+(4+5+2)-3)+(6+8)")
--- 数据结构与算法之美/栈/ext-greater-element-i.js
+++ 数据结构与算法之美/栈/ext-greater-element-i.js
@@ -10,4 +10,22 @@
  */
 var nextGreaterElement = function (nums1, nums2) {
 
+  return nums1.map(n1 => {
+    let index = nums2.findIndex(n2 => {
+      return n2 == n1
+    })
+    for (let cur = index + 1; cur < nums2.length; cur++) {
+      const curN = nums2[cur];
+      if (curN > n1) {
+        return curN
+      }
+    }
+    return -1
+  })
 };
+
+let nums1 = [4, 1, 2]
+let nums2 = [1, 3, 4, 2]
+
+
+console.log(nextGreaterElement(nums1, nums2));
