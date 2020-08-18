3974345c7274f0dacf9f84c367b38f26e05a318e
steven.ju <steven.ju@rccchina.com>
2020-08-18 09:15:41 +0800

    修改: 加入sqrt , 修改二分查找

--- 数据结构与算法之美/排序/binary-search.js
+++ 数据结构与算法之美/排序/binary-search.js
@@ -1,11 +1,11 @@
 // 二分查找
 // 数据必须是有序的数组
 
-function search(array, value) {
+function searchFirstEq(array, value) {
   let start = 0;
   let end = array.length - 1;
   while (end >= start) {
-    let mid = (start + end) / 2
+    let mid = Math.floor((start + end) / 2)
     if (array[mid] === value) {
       return mid;
     }
@@ -19,6 +19,24 @@ function search(array, value) {
 }
 
 
-let array = [-1, 3, 6, 88, 234, 672, 1231]
-let result = search(array, 234)
+function searchLastEq(array, value) {
+  let start = 0;
+  let end = array.length - 1;
+  while (end >= start) {
+    let mid = Math.floor((start + end) / 2)
+    if (array[mid] === value) {
+      return mid;
+    }
+    if (array[mid] >= value) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1
+}
+
+
+let array = [-1, 3, 6, 88, 88, 234, 672, 1231]
+let result = searchFirstEq(array, 88)
 console.log(result);
