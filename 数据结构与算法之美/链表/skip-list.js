6088e956b2118c696dd29096f35599417b6120c9
steven.ju <steven.ju@rccchina.com>
2020-08-21 08:57:15 +0800

    修改: 跳表加入注释

--- 数据结构与算法之美/链表/skip-list.js
+++ 数据结构与算法之美/链表/skip-list.js
@@ -1,2 +1,25 @@
 // 跳表
 // https://juejin.im/post/6844903446475177998
+
+
+// 双向链表
+// doubly linked list
+class DoublyLinkedList {
+
+}
+
+
+class SkipList {
+  constructor(array) {
+
+
+  }
+  // 查找
+  // 插入
+  // 删除节点时, 也要删除对应的索引
+  // 创建索引
+  // 获取连续的区间数据
+  // 跳表是通过随机函数来维护前面提到的“平衡性”
+
+}
+
