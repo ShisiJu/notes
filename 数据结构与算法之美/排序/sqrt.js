51bd2b07d107ddb6491c2a7f19609c13c2dd40fc
juss <juss@gitblit.geo-compass.com>
2020-08-23 17:21:41 +0800

    修改: 加入sqrt 二分查找求平方根

--- 数据结构与算法之美/排序/sqrt.js
+++ 数据结构与算法之美/排序/sqrt.js
@@ -1,9 +1,43 @@
 // 求一个数的平方根 求精确到小数点后 6 位。
 // 介值定理
 // 牛顿
-function calcSqrt(n) {
 
+function isOK(n, sqrtN) {
+  return Math.abs(n - sqrtN * sqrtN) <= 0.0000001
 }
 
+// 负数情况
+function compare(n, sqrtN) {
+  if (n < 1) {
+    return (n - sqrtN * sqrtN) <= 0
+  }
+  return (n - sqrtN * sqrtN) >= 0
+}
 
-calcSqrt(3)
+function calcSqrt(n, res, sqrtN) {
+  if (isOK(n, sqrtN)) {
+    return sqrtN
+  }
+  let mid = (res + sqrtN) / 2
+  if (compare(n, mid)) {
+    return calcSqrt(n, res, mid)
+  } else {
+    return calcSqrt(n, mid, sqrtN)
+  }
+}
+
+function sqrt(n) {
+  // 负数不能开平方
+  if (n <= 0) {
+    return 0
+  }
+
+  return calcSqrt(n, n, 1)
+}
+
+console.log(sqrt(-1));
+console.log(sqrt(1));
+console.log(sqrt(4));
+console.log(sqrt(0.01));
+
+
