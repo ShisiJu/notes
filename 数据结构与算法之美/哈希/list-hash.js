77542567064a3f54761daa860474af4b30f808f2
juss <juss@gitblit.geo-compass.com>
2020-08-27 22:09:11 +0800

    修改: list-hash加入hash函数, add 和 delete

--- 数据结构与算法之美/哈希/list-hash.js
+++ 数据结构与算法之美/哈希/list-hash.js
@@ -1,40 +1,105 @@
 // 链表法
-// 字符串
-// js hashing function
-// 负数取模
-// 处会报错，缘由是hashAddress = data % hashLength得到的结果是负数。为此，
-// 需要加上语句 if(hashAddress < 0) hashAddress +=hashLength; 可以有效解决该问题。
-
-// 字符串hashCode, 可能是负数
 Object.defineProperty(String.prototype, 'hashCode', {
-  value: function() {
+  value: function () {
     var hash = 0, i, chr;
     for (i = 0; i < this.length; i++) {
-      chr   = this.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
+      chr = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
       hash |= 0; // Convert to 32bit integer
     }
-    return hash;
+    return Math.abs(hash);
   }
 });
 
+class Node {
+  constructor(key, value, next = null) {
+    this.value = value;
+    this.key = key;
+    this.next = next;
+  }
+}
 
-// Test
+function deleteKey(head, key) {
+  while (head.next != null) {
+    if (head.next.key == key) {
+      head.next = head.next.next
+    } else {
+      head = head.next
+    }
+  }
+  return head
+}
+
 class ListHash {
   constructor(capacity = 16, loadFactor = 0.75) {
     this.capacity = capacity;
     this.loadFactor = loadFactor;
+    this.array = []
+    this.count = 0
   }
 
-  add() {}
-
-  delete() {}
-
-  get() {}
-
-  hash() {}
-
-  hash(key) {
-    
+  isFull() {
+    return this.loadFactor < this.count / this.capacity
   }
+
+  size() {
+    return this.count
+  }
+
+  add(key, value) {
+    // 扩容处理
+    const hashIndex = this.index(key)
+    const newNode = new Node(key, value, null)
+    this.count++
+    if (this.array[hashIndex] == null) {
+      this.array[hashIndex] = new Node(null, null, newNode)
+      return
+    }
+    const tail = this.__delete(key)
+    tail.next = newNode
+  }
+
+  index(key) {
+    return key.hashCode() % this.capacity
+  }
+
+  keyHead(key) {
+    return this.array[this.index(key)]
+  }
+
+  delete(key) {
+    __delete(key)
+  }
+
+  __delete(key) {
+    this.count--
+    // 对应index中没有数据
+    if (!this.keyHead(key)) {
+      return
+    }
+    return deleteKey(this.keyHead(key), key)
+  }
+
+  get(key) {
+    let head = this.array[this.index(key)]
+    while (head != null) {
+      head.key
+      if (head.key === key) {
+        return head.value
+      }
+      head = head.next
+    }
+    return undefined
+  }
+
+
 }
+
+// 测试用例
+let hash = new ListHash()
+
+console.assert(hash.add('kjjl', 1) === undefined, true)
+console.assert(hash.add('kjjl', 1) === undefined, true)
+console.assert(hash.add('kjjl', 1) === undefined, true)
+console.assert(hash.size() === 1, `size应该为1, 但是值却为${hash.size()}`)
+
--- 数据结构与算法之美/哈希/open-addressing.js
+++ 数据结构与算法之美/哈希/open-addressing.js
@@ -1,2 +1,42 @@
 // 开放寻址法
 // 线性探测（Linear Probing）。
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.deleted = false
+  }
+}
+class HashMap {
+  constructor(capacity) {
+    this.capacity = capacity
+    this.array = []
+    this.count = 0
+    this.loadFactor = 0.5
+  }
+
+  overCapacity() {
+    return this.loadFactor <= this.count / this.capacity
+  }
+
+  // 假设key都是数字
+  index(key) {
+    return key % this.capacity
+  }
+
+  add(key, value) {
+    const hashIndex = this.index()
+    if (this.overCapacity()) {
+
+    }
+
+    if (this.array[hashIndex] == null) {
+      this.array[hashIndex] = new Node(value)
+    } else {
+      // while(availableIndex)
+
+    }
+
+
+  }
+}
