4af973f7ebfb22bc6c66bd35be8a02e99e7802e3
steven.ju <steven.ju@rccchina.com>
2020-10-27 09:09:17 +0800

    修改: 完善dijkstra

--- 数据结构与算法之美/高级/最短路径/Dijkstra.js
+++ 数据结构与算法之美/高级/最短路径/Dijkstra.js
@@ -3,15 +3,45 @@ class SmallHeap {
     this.array = [];
   }
 
-  create_or_update(node){
-    
+  size() {
+    return this.array.length;
+  }
 
+  add(vertex) {
+    this.array.push(vertex);
+    this.sort();
+  }
+
+  poll() {
+    let first = this.array[0];
+    this.array = this.array.slice(1);
+    return first;
+  }
+
+  create_or_update(vertex) {
+    let v = this.array.find((v) => {
+      return v.vertex_index == vertex.vertex_index;
+    });
+
+    if (v) {
+      v.weight = vertex.weight;
+      this.sort();
+    } else {
+      this.add(vertex);
+    }
   }
 
   sort() {
     this.array.sort(function (a, b) {
-      return a - b;
+      return a.weight - b.weight;
     });
+  }
+}
+
+class Vertex {
+  constructor(vertex_index, weight) {
+    this.vertex_index = vertex_index;
+    this.weight = weight;
   }
 }
 
@@ -45,7 +75,32 @@ class Graph {
   }
 
   dijkstra(start, end) {
-    // 初始化所有的顶点
+    // 初始化所有的顶点的 min_dist
+    const min_dist = new Array(this.vertex_num).fill(Number.MAX_VALUE);
+    // 给 start 点的min_dist 设置为0
+    min_dist[start] = 0;
+    // small_heap 加入 start点
+    const small_heap = new SmallHeap();
+    // 遍历 small_heap  直到它为空
+    small_heap.create_or_update(new Vertex(start, 0));
+    while (small_heap.size() != 0) {
+      let cur_v = small_heap.poll();
+      let cur_i =cur_v.vertex_index
+      // TODO: 这里有些不太懂
+      if(cur_i == end){
+        break 
+      }
+      let cur_weight = min_dist[cur_i];
+      for (let i = 0; i < this.adjacent_table[cur_i].length; i++) {
+        const next_edge = this.adjacent_table[cur_i][i];
+        const comp_weight = cur_weight + next_edge.weight;
+        if (comp_weight < min_dist[next_edge.end]) {
+          min_dist[next_edge.end] = comp_weight
+          small_heap.create_or_update(new Vertex(next_edge.end, comp_weight));
+        }
+      }
+    }
+    return min_dist
   }
 }
 
@@ -61,4 +116,7 @@ let ar = [
   [4, 5, 10],
 ];
 
+
 g.add_edge_by_array(ar);
+
+g.dijkstra(0,5)
