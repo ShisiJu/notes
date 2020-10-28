f698e10b34aab7ef58eb435c9f47176c49d9fbda
steven.ju <steven.ju@rccchina.com>
2020-10-28 09:07:57 +0800

    修改: 加入本周计划

---
+++ 数据结构与算法之美/图/leetcode/find-k-pairs-with-smallest-sums.js
@@ -0,0 +1,12 @@
+// https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums/
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kSmallestPairs = function(nums1, nums2, k) {
+
+};
---
+++ 数据结构与算法之美/图/leetcode/jump-game-iii.js
@@ -0,0 +1,11 @@
+// https://leetcode-cn.com/problems/jump-game-iii/
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+var canReach = function(arr, start) {
+
+};
